e723869476c9ea34d7bd5484ab94c8d52e01b428
KvotheBG <45050347+KvotheBG@users.noreply.github.com>
2019-03-16 19:07:33 +0200

    Add files via upload

---
+++ hangman/include/footer.php
@@ -0,0 +1,13 @@
+<div class="col-lg-12 footer">
+	<div class="col-lg-3">
+		<h2>User:</h2>
+	</div>
+	<div class="col-lg-2">
+		<h2>Rank:</h2>
+	</div>
+	<div class="col-lg-2">
+		<h2>Score:</h2>
+	</div>
+</div>
+</body>
+</html>
